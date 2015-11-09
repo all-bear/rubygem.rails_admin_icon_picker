b7e280d5b273baa0f3ac2a8be2e4ef793a7e4596
Oleh Birjukov <ol.birjukov@gmail.com>
2015-11-09 16:28:13 +0200

    fix styles

--- app/assets/javascripts/rails_admin/icon_picker.js
+++ app/assets/javascripts/rails_admin/icon_picker.js
@@ -1,5 +1,8 @@
 //= require 'rails_admin/fontawesome-iconpicker/fontawesome-iconpicker.min'
 
 jQuery(document).on('rails_admin.dom_ready', function () {
-    $('[data-icon-picker-field]').iconpicker({hideOnSelect: true})
+    $('[data-icon-picker-field]').each(function(i, el) {
+        $(el).iconpicker({hideOnSelect: true}).parent().parent().css('z-index', 5);
+    });
 });
+
