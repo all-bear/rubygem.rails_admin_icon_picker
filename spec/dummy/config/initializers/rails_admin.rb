6b367db9706868a4c2e80c1e477b2930f8d3eabc
Oleh Birjukov <ol.birjukov@gmail.com>
2015-11-10 00:03:04 +0200

    commit rails_admin configs in dummy (use unit tests)

--- spec/dummy/config/initializers/rails_admin.rb
+++ spec/dummy/config/initializers/rails_admin.rb
@@ -1,21 +1,5 @@
+=begin
 RailsAdmin.config do |config|
-
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
   config.actions do
     dashboard # mandatory
     index # mandatory
@@ -38,3 +22,4 @@ RailsAdmin.config do |config|
     end
   end
 end
+=end
